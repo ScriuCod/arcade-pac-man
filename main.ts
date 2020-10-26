c6d2e89fbd2d87ec68ca60dc6e01943e27738e70
Nicolae Gabriel <ngabyn@users.noreply.github.com>
2020-10-26 15:00:40 +0200

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,30 +1,88 @@
+namespace SpriteKind {
+    export const Ghost = SpriteKind.create()
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Ghost, function (sprite, otherSprite) {
+    otherSprite.destroy()
+    info.changeLifeBy(-1)
+    info.changeScoreBy(-25)
+})
+function createFood () {
+    for (let index = 0; index < 20; index++) {
+        sprFood = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . 5 5 5 5 5 . . . . . . 
+            . . . . . 5 5 5 5 5 . . . . . . 
+            . . . . . 5 5 5 5 5 . . . . . . 
+            . . . . . 5 5 5 5 5 . . . . . . 
+            . . . . . 5 5 5 5 5 . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Food)
+        tiles.placeOnRandomTile(sprFood, sprites.castle.tilePath5)
+    }
+}
+function createEnemeis () {
+    for (let index = 0; index < 4; index++) {
+        sprGhost = sprites.create(img`
+            . . . f f f f f f f f f f . . . 
+            . . f a a a a a a a a a f f . . 
+            . f a a a a a a a a a a a a f . 
+            f a a a f f a a f f a a a a a f 
+            f a a a f a a a f a a a a a a f 
+            f a a a a a a a a a a a a a a f 
+            f a a a a a a a a a a a a a a f 
+            f a a a a a a a a a a a a a a f 
+            f a a a a f a f a f a a a a a f 
+            f a a a f a f a f a f a a a a f 
+            f a a a a a a a a a a a a a a f 
+            f a a a a a a a a a a a a a a f 
+            f a a a a a a a a a a a a a a f 
+            f a a a a a a a a a a a a a a f 
+            f a a a a a a a a a a a a a a f 
+            . f . f . f . f . f . f . f . f 
+            `, SpriteKind.Ghost)
+        tiles.placeOnRandomTile(sprGhost, sprites.castle.tilePath5)
+        sprGhost.setFlag(SpriteFlag.StayInScreen, true)
+        sprGhost.setFlag(SpriteFlag.BounceOnWall, true)
+        sprGhost.setVelocity(30, 30)
+    }
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    otherSprite.destroy(effects.fire, 200)
+    info.changeScoreBy(1)
+})
+let sprGhost: Sprite = null
+let sprFood: Sprite = null
+info.setScore(5)
+info.setLife(5)
+tiles.setTilemap(tilemap`level`)
 let sprPacMan = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . 5 5 5 5 5 5 5 5 . . . 
-    . . . 5 5 5 e e 5 5 5 5 5 5 . . 
-    . . 5 5 5 5 e e 5 5 5 5 5 5 5 . 
-    . . 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
-    . 5 5 5 5 5 5 5 5 5 5 5 5 5 . . 
-    . 5 5 5 5 5 5 5 5 5 5 5 . . . . 
-    . 5 5 5 5 5 5 5 5 5 . . . . . . 
-    . 5 5 5 5 5 5 5 . . . . . . . . 
-    . 5 5 5 5 5 5 5 5 . . . . . . . 
-    . 5 5 5 5 5 5 5 5 5 . . . . . . 
-    . . 5 5 5 5 5 5 5 5 5 5 . . . . 
-    . . 5 5 5 5 5 5 5 5 5 5 5 . . . 
-    . . . 5 5 5 5 5 5 5 5 5 5 5 . . 
-    . . . . . 5 5 5 5 5 5 5 5 . . . 
-    . . . . . . . . . . . . . . . . 
+    . . . . 5 5 5 5 5 5 5 5 . . . . 
+    . . 5 5 5 5 5 5 5 5 5 5 5 5 . . 
+    . 5 5 5 5 5 5 5 5 5 5 5 5 5 4 . 
+    . 5 5 5 5 e e 5 5 5 5 5 5 4 . . 
+    5 5 5 5 5 e e 5 5 5 5 5 4 . . . 
+    5 5 5 5 5 5 5 5 5 5 5 4 . . . . 
+    5 5 5 5 5 5 5 5 5 5 4 . . . . . 
+    5 5 5 5 5 5 5 5 5 4 . . . . . . 
+    5 5 5 5 5 5 5 5 4 . . . . . . . 
+    5 5 5 5 5 5 5 5 5 4 . . . . . . 
+    5 5 5 5 5 5 5 5 5 5 4 . . . . . 
+    5 5 5 5 5 5 5 5 5 5 5 4 . . . . 
+    . 5 5 5 5 5 5 5 5 5 5 5 4 . . . 
+    . 5 5 5 5 5 5 5 5 5 5 5 5 4 . . 
+    . . 5 5 5 5 5 5 5 5 5 5 5 5 . . 
+    . . . . 5 5 5 5 5 5 5 5 . . . . 
     `, SpriteKind.Player)
-tiles.setTilemap(tiles.createTilemap(hex`0a00080001080808080808080802090b0b0b0a0b0b0d0b07090d0a0a0a0a0a0a0b07090b0a0b0b0b0c0b0b07090b0a0a0a0a0a0a0a07090b0b0a0b0b0a0b0b070306060606060606060405050505050505050505`, img`
-    . . . . . . . . . . 
-    . 2 2 2 . 2 2 . 2 . 
-    . . . . . . . . 2 . 
-    . 2 . 2 2 2 . 2 2 . 
-    . 2 . . . . . . . . 
-    . 2 2 . 2 2 . 2 2 . 
-    . . . . . . . . . . 
-    2 2 2 2 2 2 2 2 2 2 
-    `, [myTiles.transparency16,sprites.castle.tilePath1,sprites.castle.tilePath3,sprites.castle.tilePath7,sprites.castle.tilePath9,sprites.castle.tileGrass1,sprites.castle.tilePath8,sprites.castle.tilePath6,sprites.castle.tilePath2,sprites.castle.tilePath4,sprites.castle.tilePath5,sprites.dungeon.floorLight0,myTiles.tile1,myTiles.tile2], TileScale.Sixteen))
 tiles.placeOnRandomTile(sprPacMan, myTiles.tile1)
 controller.moveSprite(sprPacMan, 100, 100)
+createFood()
+createEnemeis()
